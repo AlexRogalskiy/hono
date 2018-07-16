bb7414a38b6ce6f2e235f60aceefaf9a3049f580
Dejan Bosanac <dejan@nighttale.net>
2018-07-16 08:58:42 +0200

    initial implementation of dummy device registry

---
+++ services/device-registry/src/main/java/org/eclipse/hono/deviceregistry/DummyTenantService.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat Inc and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat - initial creation
+ *******************************************************************************/
+package org.eclipse.hono.deviceregistry;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
+import org.eclipse.hono.service.tenant.BaseTenantService;
+import org.eclipse.hono.util.TenantObject;
+import org.eclipse.hono.util.TenantResult;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.net.HttpURLConnection;
+
+/**
+ *
+ */
+@Service
+@Primary
+public class DummyTenantService extends BaseTenantService<Object> {
+
+    @Override
+    public void setConfig(final Object configuration) {
+
+    }
+
+    @Override
+    public void get(final String tenantId, final Handler<AsyncResult<TenantResult<JsonObject>>> resultHandler) {
+        final TenantObject tenant = new TenantObject();
+        tenant.setTenantId(tenantId);
+        tenant.setEnabled(true);
+        resultHandler.handle(Future.succeededFuture(TenantResult.from(HttpURLConnection.HTTP_OK,
+                JsonObject.mapFrom(tenant),
+                null)));
+    }
+}
